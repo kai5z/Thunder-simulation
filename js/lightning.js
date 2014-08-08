80ce52eaea3e4cfed6f5f5a674f4469786c36826
Kai Saksela <kai@kaistale.com>
2014-08-08 13:04:00 +0300

    First commit

---
+++ js/lightning.js
@@ -0,0 +1,202 @@
+/**
+ * Copyright Kai Saksela 2014
+ *
+ * This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+var LightningGenerator =
+{
+
+    //Distance between two points
+    dist: function(p1,p2)
+    {
+        return Math.sqrt(Math.pow(p2.x-p1.x,2)+Math.pow(p2.y-p1.y,2)+Math.pow(p2.z-p1.z,2))
+    },
+
+    //Normal distribution
+    normal: function(mean, stdev)
+    {
+        function rnd_snd() {
+            return (Math.random() * 2 - 1) + (Math.random() * 2 - 1) + (Math.random() * 2 - 1);
+        }
+
+        return rnd_snd() * stdev + mean;
+    },
+
+    //normalize vector to unit vector
+    unit: function(v) {
+        v_len = Math.sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+        v.x = v.x / v_len;
+        v.y = v.y / v_len;
+        v.z = v.z / v_len;
+
+        return v;
+    },
+
+    //Cross product of two vectors
+    cross: function(u, v) {
+        return {
+            x: u.y * v.z - u.z * v.y,
+            y: u.z * v.x - u.x * v.z,
+            z: u.x * v.y - u.y * v.x
+        };
+    },
+
+    generate_lightning_point: function(p, v1, v2, v3, z_delta) {
+        //Variables loosely based on Rakov et al. ("Lightning, physics and effects") p. 378
+        var segment = {
+            length: this.normal(40, 10) * 65 + 5,
+            theta: this.normal(16, 16) / 180.0 * Math.PI,
+            phi: Math.random() * 2.0 * Math.PI
+        };
+
+        //Constraints, 1
+        if (segment.length < 3.5) segment.length = 3.5;
+        if (segment.length > 70.0) segment.length = 70.0;
+        if (segment.theta > Math.PI) segment.theta = Math.PI;
+        if (segment.theta < 0) segment.theta = -segment.theta;
+
+        //New point based on segment
+        var new_p = {x: segment.length * Math.sin(segment.theta) * Math.cos(segment.phi),
+            y: segment.length * Math.sin(segment.theta) * Math.sin(segment.phi),
+            z: segment.length * Math.cos(segment.theta)};
+
+        //Rotate and translate to its correct place (relative to prev)
+        var temp_p = {x: 0, y: 0, z: 0};
+        temp_p.x = p.x + new_p.x * v1.x + new_p.y * v2.x + new_p.z * v3.x;
+        temp_p.y = p.y + new_p.x * v1.y + new_p.y * v2.y + new_p.z * v3.y;
+        temp_p.z = p.z + z_delta + new_p.x * v1.z + new_p.y * v2.z + new_p.z * v3.z; //Add small trend upwards
+        new_p = temp_p;
+
+        //Constraints, 2
+        if (new_p.z < 2.0) new_p.z = 2.0 + Math.random() * 2.0; //It can't go through the ground in our sim
+
+        //Update vectors describing previous segment
+        var v3 = {x: new_p.x - p.x,
+            y: new_p.y - p.y,
+            z: new_p.z - p.z};
+        v3 = this.unit(v3);
+        var v1, v2;
+        if (v3.x < 0.95) {
+            v2 = this.cross(v3, {x: 1.0, y: 0, z: 0});
+        }
+        else {
+            v2 = this.cross(v3, {x: 0, y: 1.0, z: 0});
+        }
+        v2 = this.unit(v2);
+        v1 = this.cross(v3, v2);
+        v1 = this.unit(v1);
+
+        return {p: new_p, v1: v1, v2: v2, v3: v3, length: segment.length};
+    },
+
+    generate_branch: function(pos, length, direction, branch_p) {
+        var lightning_points = [[]];
+        var p = {x: pos.x, y: pos.y, z: pos.z};
+        lightning_points[0].push({x: p.x, y: p.y, z: p.z});
+
+        //Random start dir
+        var v3 = {x: Math.random(), y: Math.random(), z: direction};
+        v3 = this.unit(v3);
+        var v1, v2;
+        if (v3.x < 0.95) {
+            v2 = this.cross(v3, {x: 1.0, y: 0, z: 0});
+        }
+        else {
+            v2 = this.cross(v3, {x: 0, y: 1.0, z: 0});
+        }
+        v2 = this.unit(v2);
+        v1 = this.cross(v3, v2);
+        v1 = this.unit(v1);
+
+        var branch_length = 0;
+
+        while (branch_length < length) {
+            if (Math.random() < branch_p) {
+                console.log("Adding " + lightning_points.length);
+                lightning_points = lightning_points.concat(this.generate_branch(p, Math.random() * 0.5 * length, direction, branch_p));
+                console.log("Now " + lightning_points.length);
+                console.log(lightning_points);
+            }
+
+            var gen_data = this.generate_lightning_point(p, v1, v2, v3, direction);
+
+            p = gen_data.p;
+            v1 = gen_data.v1;
+            v2 = gen_data.v2;
+            v3 = gen_data.v3;
+            branch_length += gen_data.length;
+
+            lightning_points[0].push({x: p.x, y: p.y, z: p.z}); //Remember, this needs postprocessing
+        }
+
+        return lightning_points;
+    },
+
+    generate_lightning: function(x_pos, y_pos, height, width, branch_p) {
+        var lightning_points, p, v1, v2, v3, branches;
+        var branches = [];
+
+        function reset_lightning() {
+            //Array of points
+            lightning_points = [];
+            branches = [];
+
+            //Current position
+            p = {x: x_pos, y: y_pos, z: 0};
+            lightning_points.push({x: p.x, y: p.y, z: p.z});
+
+            //Previous segment coordinate system for defining new point
+            v1 = {x: 1.0, y: 0, z: 0};
+            v2 = {x: 0, y: 1.0, z: 0};
+            v3 = {x: 0, y: 0, z: 1.0};
+        }
+
+        reset_lightning();
+
+        var branch_direction = Math.random() * 3.0 - 1.5;
+        while (p.z < height) {
+            if (Math.random() < branch_p)
+            {
+                var branch = this.generate_branch(p, Math.random() * 0.5 * height, branch_direction, branch_p);
+                branches = branches.concat(branch);
+            }
+            var gen_data = this.generate_lightning_point(p, v1, v2, v3, 1.0);
+
+            p = gen_data.p;
+            v1 = gen_data.v1;
+            v2 = gen_data.v2;
+            v3 = gen_data.v3;
+
+            lightning_points.push(p);
+
+            //If the lightning is invalid, restart the process
+            if (p.x < x_pos - width / 2 || p.x > x_pos + width / 2 ||
+                p.y < y_pos - width / 2 || p.y > y_pos + width / 2) {
+                reset_lightning();
+            }
+        }
+
+        //Generate the "crown" inside the cloud
+        //Earth's electrical environment (geophysics study committee), p. 33
+        var top = [];
+        for (var i = 0; i < 3 + Math.random() * 5; i++) {
+            top = top.concat(this.generate_branch(p, Math.random() * 0.5 * height, 0.0, 0.0));
+        }
+
+
+        return {main: lightning_points, branches: branches, top: top};
+    }
+
+}
+
---
+++ js/main.js
@@ -0,0 +1,244 @@
+/**
+ * Copyright Kai Saksela 2014
+ *
+ * This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var canvas = document.getElementById('lightning');
+var plot = document.getElementById('plot');
+
+//MAIN
+(function() {
+    var lightning, listener;
+    var mouse_dragging = false;
+    var lWorker = new Worker("js/lightning_worker.js");
+    lWorker.addEventListener('message', function(e){lWorker_message(e);}, false);
+    var progressBar = new ProgressBar("generator_progress", {'width':'200px', 'height':'3px'});
+    $("#generator_progress").hide();
+
+    var settings = {
+        position: 2000,
+        scale: 25.0,
+        Fs: 22000,
+        cs: 340.0,
+        clip: true
+    };
+
+    function convolve(x,h)
+    {
+        var y = new Array(x.length + h.length - 1);
+        for (var i = 0; i < y.length; i++ ) {
+            y[i] = 0;
+        }
+        for (var i = 0; i < x.length; i++ )
+        {
+            //if(i % 1000 == 0)
+            //    console.log((i/ y.length*100).toFixed(1) + "%");
+
+            for (var j = 0; j < h.length; j++ )
+            {
+                y[i+j] += x[i] * h[j];    // convolve: multiply and accumulate
+            }
+        }
+        return y;
+    }
+
+    /*
+    function hanning(samples)
+    {
+        var s = new Array(samples);
+        for(var i = 0; i < s.length; i++)
+        {
+            s[i] = 0.5*(1-Math.cos(2*Math.PI*i/(samples-1)));
+        }
+        return s;
+    }*/
+    function listen()
+    {
+        if(!this.thunder)
+        {
+            alert("You need to generate the thunder first!");
+        }
+
+        //Play sound
+        //-------------
+        var sound = new Array(this.thunder.length);
+        for(var i = 0; i < this.thunder.length; i++)
+        {
+            sound[i] = this.thunder[i] * (settings.clip ? 2.0 : 1.0);
+            if(Math.abs(sound[i]) > 1.0)
+            {
+                sound[i] = sound[i]/ Math.abs(sound[i]);
+            }
+            sound[i] = 32767*sound[i];
+        }
+
+        var audio = new Audio(); // create the HTML5 audio element
+        var wave = new RIFFWAVE(); // create an empty wave file
+
+        wave.header.sampleRate = settings.Fs;
+        wave.header.numChannels = 1;
+        wave.header.bitsPerSample = 16;
+        wave.Make(sound); // make the wave file
+        audio.src = wave.dataURI; // set audio source
+        audio.play(); // we should hear two tones one on each speaker
+
+    }
+
+    function redraw()
+    {
+
+        var pts = lightning.main;
+        var ctx = canvas.getContext('2d');
+        var height = canvas.height;
+        var scale = settings.scale;
+
+        ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
+
+        //Main
+        ctx.beginPath();
+        ctx.moveTo(pts[0].x / scale, height-pts[0].z / scale);
+        for(var i = 1; i < pts.length; i++)
+        {
+            //ctx.arc(pts[i].x / scale, pts[i].z / scale, 2.0, 0, 2 * Math.PI, false);
+            ctx.lineTo(pts[i].x / scale, height-pts[i].z / scale);
+        }
+        ctx.lineWidth = 40.0 / scale;
+        ctx.strokeStyle = '#FCFDFF';
+        ctx.stroke();
+
+        //Branches
+
+        for(var i = 0; i < lightning.branches.length; i++)
+        {
+            ctx.beginPath();
+            var pts = lightning.branches[i];
+            for(var j = 0; j < pts.length; j++)
+            {
+                ctx.lineTo(pts[j].x / scale, height-pts[j].z / scale);
+            }
+            ctx.lineWidth = 20.0 / scale;
+            ctx.strokeStyle = '#ECEDEF';
+            ctx.stroke();
+        }
+
+        //Top
+        for(var i = 0; i < lightning.top.length; i++)
+        {
+            ctx.beginPath();
+            var pts = lightning.top[i];
+            for(var j = 0; j < pts.length; j++)
+            {
+                ctx.lineTo(pts[j].x / scale, height-pts[j].z / scale);
+            }
+            ctx.lineWidth = 20.0 / scale;
+            ctx.strokeStyle = '#5C5D5F';
+            ctx.stroke();
+        }
+
+        //Listener
+        ctx.beginPath();
+        ctx.moveTo(listener.x/scale,canvas.height);
+        ctx.lineTo(listener.x/scale-(mouse_dragging ? 8 : 6),canvas.height-(mouse_dragging ? 10 : 7));
+        ctx.lineTo(listener.x/scale+(mouse_dragging ? 8 : 6),canvas.height-(mouse_dragging ? 10 : 7));
+        ctx.lineTo(listener.x/scale,canvas.height);
+        ctx.fillStyle = '#DDDDFF';
+        ctx.fill();
+    }
+
+
+    $(canvas).mousedown(function(event) {
+
+        console.log("Mouse down");
+        mouse_dragging = true;
+        listener.x = event.offsetX*settings.scale;
+        $("#lightning_distance").text(Math.abs((listener.x-lightning.main[0].x)/1000).toFixed(2));
+        redraw();
+    });
+    $(document).mouseup(function(event) {
+        console.log("Mouse up");
+        if(mouse_dragging)
+        {
+            mouse_dragging = false;
+            redraw();
+        }
+    });
+    $(canvas).mousemove(function(event) {
+        if(mouse_dragging) {
+            if(event.offsetX > canvas.width || event.offsetX < 0)
+                return;
+            listener.x = event.offsetX*settings.scale;
+            $("#lightning_distance").text(Math.abs(LightningGenerator.dist(listener,lightning.main[0])/1000).toFixed(2));
+            redraw();
+        }
+    });
+
+    $("#generate").click(function(){
+        lightning = LightningGenerator.generate_lightning(2000,2000,4500,4500,0.025);
+        redraw();
+    });
+
+    var lWorker_message = function(e)
+    {
+        if(e.data.process)
+        {
+            progressBar.setPercent(Math.round(e.data.process));
+        }
+        if(e.data.thunder)
+        {
+            //Add data
+            this.thunder = e.data.thunder;
+
+            //Plot waveform
+            var plt = plot.getContext('2d');
+            plt.clearRect(0,0,plot.width, plot.height);
+
+            plt.beginPath();
+            plt.moveTo(0,plot.height/2);
+            for(var i = 1; i < this.thunder.length; i++)
+            {
+                plt.lineTo(i/this.thunder.length*plot.width,plot.height/2+plot.height/2*thunder[i]);
+            }
+
+            plt.strokeStyle = '#000000';
+            plt.stroke();
+            console.log("Done");
+
+            $("#generate_thunder").show();
+            $("#generator_progress").hide();
+        }
+    }
+    $("#generate_thunder").click(function(){
+        $("#generate_thunder").hide();
+        $("#generator_progress").show();
+
+        if(this.pts) delete this.pts;
+        if(this.thunder) delete this.thunder;
+        lWorker.postMessage({'lightning': lightning, 'listener': listener, 'settings': settings});
+
+    });
+    $("#listen").click(function(){
+        listen();
+    });
+
+    if (canvas.getContext){
+        lightning = LightningGenerator.generate_lightning(2000,2000,4500,4500,0.025);
+        listener = {x: 3400, y: 2000, z: 0};
+        $("#lightning_distance").text(Math.abs(LightningGenerator.dist(listener,lightning.main[0])/1000).toFixed(2));
+        redraw();
+    } else {
+        alert("You need a better browser to run the lightning simulation!");
+      // canvas-unsupported code here
+    }
+})();
